8b041b325f7ee8f0f2161728fe61caacdf7ff07d
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-18 15:21:37 -0400

    image and starting some typography

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Navigation from './components/Navigation';
+import girlPicture from './Assets/girlPicture.svg';
+import Typography from '@material-ui/core/Typography';
 
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Typography variant="h2">
+      Your product's trends can be hard to follow, <strong>Let us help.</strong>
+      </Typography>
+      <img src={girlPicture}></img>
     </div>
   );
 }
