6d0c45cd0e8726c349b99ad8913e4e43bcce1329
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-20 10:24:31 -0400

    used grid to center the navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,35 +6,55 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(4),
   },
   title: {
     flexGrow: 1,
   },
+  Tab: {
+    textTransform: "none",
+  }
+
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
+      <AppBar position="fixed">
+      <Grid container direction="row" justify="center">
+        <Toolbar justify="center" >
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Trendr by AI Consult
           </Typography>
-          <Button variant="contained" color="primary">How it works</Button>
-          <Button variant="contained" color="primary">Pricing</Button>
-          <Button variant="contained" color="secondary">Demo</Button>
+    
+          <Tabs>
+          <Tab className={classes.Tab} label="How it works" {...a11yProps(0)}></Tab>
+          <Tab className={classes.Tab} label="Pricing" {...a11yProps(1)}></Tab>
+          </Tabs>
+          <Button variant="contained" color="secondary" className={classes.Tab}{...a11yProps(2)}>Demo</Button>
+      
         </Toolbar>
+        </Grid>
+     
       </AppBar>
     </div>
   );
