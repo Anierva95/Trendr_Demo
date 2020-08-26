27551d9a9e1f894700fbcbf4c7ca8d47326103d7
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-25 23:02:02 -0400

    add contact page and fixed up some imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import Navigation from "./components/Navigation";
-import Banner from "./components/Banner";
-import BannerCTA from "./components/BannerCTA";
-import girlPicture from "./Assets/girlPicture.svg";
-import Footer from './components/footer';
+
 import Home from './pages/Home';
 import Demo from './pages/Demo'
 
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./components/theme";
-import ImportExportIcon from "@material-ui/icons/ImportExport";
-import AssessmentIcon from "@material-ui/icons/Assessment";
-import AdbIcon from "@material-ui/icons/Adb";
 
 const useStyles = makeStyles((theme) => ({
   girlImage: {
